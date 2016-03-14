67e127f8cf4645b8d8b9de2194a910dd30eb6745
filomarcello <marcello.filopanti@gmail.com>
2016-03-14 11:32:19 +0100

    initial commit for class views switching

--- patients/views.py
+++ patients/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-from django.views.generic import View
+from django.views.generic import TemplateView
 from django.shortcuts import render
 
 from parag.constants import VIP_USERS
@@ -25,4 +25,17 @@ def help(request):
 
 def contacts(request):
     return render(request, 'patients/contacts.html')
+
+
+# class views
+class BaseView(TemplateView):
+
+    template_name = 'main.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user'] = request.user
+
+
+        return
 
