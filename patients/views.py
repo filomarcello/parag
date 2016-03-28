5d3727e26a6ac069eb9c69414c817aed8996994f
filomarcello <marcello.filopanti@gmail.com>
2016-03-28 22:57:43 +0200

    view classes have now login required

--- patients/views.py
+++ patients/views.py
@@ -1,16 +1,13 @@
-from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
 from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
-from django.shortcuts import render
-
-from parag.constants import VIP_USERS
 
 
 class BaseView(TemplateView):
 
-    @method_decorator(login_required)
-    def dispatch(self, request, *args, **kwargs):
-        return super(BaseView, self).dispatch(request, *args, **kwargs)
+    @method_decorator(staff_member_required)
+    def dispatch(self, *args, **kwargs):
+        return super(BaseView, self).dispatch(*args, **kwargs)
 
 class MainView(BaseView):
     template_name = 'patients/main.html'
